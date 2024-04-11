d6105d10082d24d523943fd992fe882b016f2855
gangabiswa <Gbiswa@pgh.year.org>
2024-04-11 13:54:42 -0400

    initial commit

---
+++ java-development/workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.security.cert.URICertStoreParameters;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("rice");
+        System.out.println("water");
+        System.out.println("what");;
+    }
+}
