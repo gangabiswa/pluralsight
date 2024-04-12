7f8ad2de9fae00ab11932ddee43d8a5c0eafef9b
gangabiswa <Gbiswa@pgh.year.org>
2024-04-12 11:13:49 -0400

    completed mod 3 - exercise 4 - payroll calculator

---
+++ java-development/workbook-1/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import javax.lang.model.element.Name;
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[]args) {
+        Scanner potatoScanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = potatoScanner.nextLine();
+
+        System.out.println("Enter hours worked: ");
+        double hours = potatoScanner.nextDouble();
+
+        System.out.println("Enter pay rate: ");
+        double rate = potatoScanner.nextDouble();
+
+       double grossPay = hours * rate;
+
+        System.out.println("Name: " + name + "Gross pay: $" + grossPay);
+
+
+    }
+}
